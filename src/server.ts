270afbad7310a0022fe9b6fa6faa35696683f6c9
Kakon Debnath <kakondebnath96@gmail.com>
2023-11-22 21:54:02 +0600

    add mongoose connection and some eslint rule add

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
+import mongoose from 'mongoose';
 import app from './app';
+import config from './app/config';
 
-const port = process.env.PORT || 5000;
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
-});
+main();
