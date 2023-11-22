e0e976a9d8f1333e0170776739944a4f9191ec1b
Kakon Debnath <kakondebnath96@gmail.com>
2023-11-22 23:11:03 +0600

    add user interface and user model

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,24 @@
+import { TUser } from './user.interface';
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema<TUser>({
+  userId: { type: Number, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  fullName: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean, required: true },
+  hobbies: { type: [String], required: true },
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+});
+
+// Create the Mongoose model for the user
+export const User = mongoose.model<TUser>('User', UserSchema);
